1aaf1ab3e0812dff4c2b765938c8a994778728d0
joshia5 <joshia5@rpi.edu>
2020-01-14 13:12:13 -0500

    working 1 order up adj and tags

--- adjacencies.cpp
+++ adjacencies.cpp
@@ -17,6 +17,14 @@ int main(int argc, char** argv) {
   Omega_h::binary::read(inmesh, lib.world(), &mesh);
   const auto dim = mesh.dim();
 
+  if(!rank) {
+    fprintf(stderr, "mesh <v e f r> %d %d %d %d\n",
+        mesh.nglobal_ents(0),
+        mesh.nglobal_ents(1),
+        mesh.nglobal_ents(2),
+        dim == 3 ? mesh.nglobal_ents(3) : 0);
+  }
+
   auto n_edge = mesh.nedges();
   auto edge2verts = mesh.get_adj(1,0);
   assert(edge2verts.ab2b.size() == 2*n_edge);
@@ -31,17 +39,29 @@ int main(int argc, char** argv) {
     assert(cell2faces.ab2b.size() == 4*n_cell);
     auto face2cell = mesh.ask_up(2,3);
     assert(face2cell.ab2b.size() == 4*n_cell);
+    auto a2ab = face2cell.a2ab;
+    int lastVal;
+    int lastIndex = a2ab.size() - 1;
+    Kokkos::deep_copy(lastVal,Kokkos::subview(a2ab.view(),lastIndex));
+    cout << " lastValue in index array f2c " << lastVal << " ncell " << 4*n_cell << " last index " << lastIndex << endl;
+    assert(lastVal <= 4*n_cell);
   }
 
   auto vert2edges = mesh.ask_up(0,1);
   auto a2ab = vert2edges.a2ab;
   int lastVal;
-  Kokkos::deep_copy(lastVal,Kokkos::subview(a2ab.view(),69104));
+  int lastIndex = a2ab.size() - 1;
+  Kokkos::deep_copy(lastVal,Kokkos::subview(a2ab.view(),lastIndex));
+  cout << " lastValue in index array v2e " << lastVal << " nedge " << 2*n_edge << " last index " << lastIndex << endl;
   assert(lastVal <= 2*n_edge);
   assert(vert2edges.ab2b.size() == 2*n_edge);
 
   auto edge2face = mesh.ask_up(1,2);
-//  cout << "here  " << edge2face.ab2b.size() << "face " << n_face << "edge " << n_edge << endl;
+  a2ab = edge2face.a2ab;
+  lastIndex = a2ab.size() - 1;
+  Kokkos::deep_copy(lastVal,Kokkos::subview(a2ab.view(),lastIndex));
+  cout << " lastValue in index array e2f " << lastVal << " nface " << 3*n_face << " last index " << lastIndex << endl;
+  assert(lastVal <= 3*n_face);
   assert(edge2face.ab2b.size() == 3*n_face);
   return 0;
 }
--- tags.cpp
+++ tags.cpp
@@ -13,14 +13,15 @@ int main(int argc, char** argv) {
   Omega_h::Mesh mesh(&lib);
   Omega_h::binary::read(inmesh, lib.world(), &mesh);
   const auto dim = mesh.dim();
-  auto nvert = mesh.nverts(); 
 
-  double gravity[nvert] = {0.0};
-  //double gravity[nvert][dim] = {0.0};
-  mesh.add_tag<double>(0, "gravity", 0);
-  mesh.set_tag<double>(0, "gravity", gravity);
-  Omega_h::binary::write("/users/joshia5/omegah_examples/new_mesh/tag.osh", &mesh);
- // mesh.remove_tag(0, "gravity");
+  auto nvert = mesh.nverts(); 
+  mesh.add_tag<Omega_h::Real>(0, "gravity", 1);
+  Omega_h::Write<Omega_h::Real> gravityArray(nvert, 9.81, "gravityArray");
+  Omega_h::Read<Omega_h::Real> gravityArray_r(gravityArray);
+  mesh.set_tag<Omega_h::Real>(0, "gravity", gravityArray_r);
+  Omega_h::binary::write("./tag.osh", &mesh);
+  mesh.remove_tag(0, "gravity");
+  Omega_h::binary::write("./no_tag.osh", &mesh);
 
   return 0;
 }
