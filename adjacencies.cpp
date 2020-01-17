281034f7399b160275162e6c9ea99f5c6b8ba0bd
joshia5 <joshia5@rpi.edu>
2020-01-17 17:55:44 -0500

    with second order v2v adjacenceies

--- adjacencies.cpp
+++ adjacencies.cpp
@@ -28,23 +28,33 @@ int main(int argc, char** argv) {
   auto n_edge = mesh.nedges();
   auto edge2verts = mesh.get_adj(1,0);
   assert(edge2verts.ab2b.size() == 2*n_edge);
+  // each edge associates with 2 vertices
 
   auto n_face = mesh.nfaces();
   auto face2edges = mesh.get_adj(2,1);
   assert(face2edges.ab2b.size() == 3*n_face);
+  // each face associates with 3 edges (all tris and tets)
 
   if (dim == 3) {
     auto n_cell = mesh.nelems();
     auto cell2faces = mesh.get_adj(3,2);
     assert(cell2faces.ab2b.size() == 4*n_cell);
+    // each cell associates with 4 faces (all tets)
     auto face2cell = mesh.ask_up(2,3);
     assert(face2cell.ab2b.size() == 4*n_cell);
+    // due to reverse graph from c2f
   }
-
-  auto vert2edges = mesh.ask_up(0,1);
-  assert(vert2edges.ab2b.size() == 2*n_edge);
 
   auto edge2face = mesh.ask_up(1,2);
   assert(edge2face.ab2b.size() == 3*n_face);
+  // due to reverse graph from f2e
+
+  auto vert2edges = mesh.ask_up(0,1);
+  assert(vert2edges.ab2b.size() == 2*n_edge);
+  // due to reverse graph from e2v
+
+  auto vert2vert = mesh.ask_star(0);
+  assert(vert2vert.ab2b.size() == 2*n_edge);
+  // counts each edge twice, once from each vertex
   return 0;
 }
