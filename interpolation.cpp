daa8ba0870027257a632faaec5a8ce69bbcb0064
joshia5 <joshia5@rpi.edu>
2020-01-20 17:56:39 -0500

    reading data of cell ID and point coordinates from user with program logic outline

--- interpolation.cpp
+++ interpolation.cpp
@@ -1,6 +1,7 @@
 #include <Omega_h_file.hpp>
 #include <Omega_h_library.hpp>
 #include <Omega_h_mesh.hpp>
+#include <initializer_list>
 using namespace std;
 
 int main(int argc, char** argv) {
@@ -15,6 +16,7 @@ int main(int argc, char** argv) {
   Omega_h::binary::read(inmesh, lib.world(), &mesh);
   const auto dim = mesh.dim();
 
+  //read cellid and point coords from user and pass to GPU
   int numPts;
   cout << " Enter no. of points to test " << endl;
   cin >> numPts;
@@ -24,9 +26,21 @@ int main(int argc, char** argv) {
     cout << " Enter element ID and coordinates for point " << i+1 << endl;
     cin >> cell_ID[i] >> point[3*i] >> point[3*i+1] >> point[3*i+2];
   }
-  std::initializer_list<Omega_h::LO> ids = cell_ID;
-  Omega_h::Read<Omega_h::LO>(ids,"elmIds");
-//  Omega_h::Write<Omega_h::Real> xyz(numPts*3);
-//  Omega_h::Write<Omega_h::LO> elmIds(numPts);
+  std::initializer_list<Omega_h::LO> ids = {cell_ID};
+  Omega_h::Read<Omega_h::LO>(ids,"elmIds");//figure out how to pass these values to initializer_list
+  /*For example:
+    Few(std::initializer_list<T> l) {
+    Int i = 0;
+    for (auto it = l.begin(); it != l.end(); ++it) {
+      new (data() + (i++)) T(*it);
+    }
+  }*/
+  //get verts of cell by  ask_verts_of or ask_elem_verts (is public in class Mesh)
+  //get coordinates of the vertices {a,b,c,d} :  look at Reals coords()
+  //routine to calculate b.centric coords for tet. and also inturn calculate is in or out of cell
+  //use "https://people.sc.fsu.edu/~jburkardt/presentations/cg_lab_barycentric_tetrahedrons.pdf"
+  // for each face get normal and calc b.centric coord for that face as (dot((p-b).n)/dot((a-b).n))
+  // example of normal auto normal = normalize(cross((b - a), (c - a))) in r3d_test.cpp;
+  //look at dot and cross in vector.hpp
   return 0;
 }
