c827724398581d2c9afb1528ce8287932757d995
joshia5 <joshia5@rpi.edu>
2020-01-22 14:00:27 -0500

    calculation for one element, ordering to be checked

--- interpolation.cpp
+++ interpolation.cpp
@@ -1,72 +1,99 @@
 #include <Omega_h_file.hpp>
 #include <Omega_h_library.hpp>
 #include <Omega_h_mesh.hpp>
-#include <initializer_list>
+#include <Omega_h_for.hpp>
 using namespace std;
+using namespace Omega_h;
 
 int main(int argc, char** argv) {
-  auto lib = Omega_h::Library(&argc, &argv);
+  auto lib = Library(&argc, &argv);
   if(argc!=2) {
     fprintf(stderr, "Usage: %s <input mesh>\n", argv[0]);
     return 0;
   }
   const auto rank = lib.world()->rank();
   const auto inmesh = argv[1];
-  Omega_h::Mesh mesh(&lib);
-  Omega_h::binary::read(inmesh, lib.world(), &mesh);
+  Mesh mesh(&lib);
+  binary::read(inmesh, lib.world(), &mesh);
   const auto dim = mesh.dim();
 
-  //read cellid and point coords from user and pass to GPU
-  int numPts;
-  cout << " Enter no. of points to test " << endl;
-  cin >> numPts;
-  int cell_ID[numPts];
+  int numPts = 1;
+  /*int cell_ID[numPts];
   double point[numPts*dim];
   for (int i=0; i<numPts; ++i) {
     cout << " Enter element ID and coordinates for point " << i+1 << endl;
-    if (dim == 3) {
-      cin >> cell_ID[i] >> point[3*i] >> point[3*i+1] >> point[3*i+2];
-    }
-    else {
-      cin >> cell_ID[i] >> point[2*i] >> point[2*i+1];
-    }      
-  }
-
-  //figure out how to pass these values to initializer_list for making it a Read object
-  //std::initializer_list<Omega_h::LO> ids = {cell_ID};
-  //Omega_h::Read<Omega_h::LO>(ids,"elmIds");
-  /*For example:
-    Few(std::initializer_list<T> l) {
-    Int i = 0;
-    for (auto it = l.begin(); it != l.end(); ++it) {
-      new (data() + (i++)) T(*it);
-    }
+    cin >> cell_ID[i] >> point[3*i] >> point[3*i+1] >> point[3*i+2];
   }*/
 
+  Read <LO> elmIds(numPts, 1, "elmIds");
+  Read <Real> p(numPts*3, 0.0, "p");
   auto cell2vert = mesh.ask_elem_verts();
   auto vert_coords = mesh.coords();
-  int cell_verts_id[(numPts*4)];
-  double cell_verts_coords[numPts*4*dim];
-  for (int j=0; j<numPts; ++j) {
-    for (int i=0; i<4; ++i) {
-      Kokkos::deep_copy(cell_verts_id[j+i],
-	Kokkos::subview(cell2vert.view(),cell_ID[j]+i));//get vertex IDs for each cell
+  int verts_per_tet = 4;
+  Write <Real> cell_verts_coords(dim*verts_per_tet, 0.0, "cell_verts_coords");
+  Write <LO> cell_verts_id(verts_per_tet, 1, "cell_verts_id");
+  Write <Real> a(dim, 0.0, "a");
+  Write <Real> b(dim, 0.0, "b");
+  Write <Real> c(dim, 0.0, "c");
+  Write <Real> d(dim, 0.0, "d");
+  Write <Real> bc(dim, 0.0, "bc");
+  Write <Real> bd(dim, 0.0, "bd");
+  Write <Real> cd(dim, 0.0, "cd");
+  Write <Real> ca(dim, 0.0, "ca");
+  Write <Real> da(dim, 0.0, "da");
+  Write <Real> ab(dim, 0.0, "ab");
+  Write <Real> p_a(dim, 0.0, "p_a");
+  Write <Real> p_b(dim, 0.0, "p_b");
+  Write <Real> p_c(dim, 0.0, "p_c");
+  Write <Real> p_d(dim, 0.0, "p_d");
+
+  auto f = OMEGA_H_LAMBDA(LO i) {
+    for (int j=0; j<verts_per_tet; ++j) {
+      cell_verts_id[j] = cell2vert[4*(elmIds[i]-1)+j];
       for (int k=0; k<dim; ++k) {
-        Kokkos::deep_copy(cell_verts_coords[j+i+k],
-	  Kokkos::subview(vert_coords.view(),cell_ID[j]+i+k));//get coords of the vertices of each cell
+        cell_verts_coords[j+k] = vert_coords[cell_verts_id[j]+k];
       }
     }
-  }
-
-  //routine to calculate b.centric coords for tet. and also inturn calculate is in or out of cell "https://people.sc.fsu.edu/~jburkardt/presentations/cg_lab_barycentric_tetrahedrons.pdf"
-  // for each face get normal and calc b.centric coord for that face as (dot((p-b).n)/dot((a-b).n))
-  int j = 0;
-  for (int i=0; i<4; ++i) { 
     for (int k=0; k<dim; ++k) {
-      n[i+k] = cross(cell_verts_coords[j+i+k+1]
+      a[k] = cell_verts_coords[k];
+      b[k] = cell_verts_coords[k+3];
+      c[k] = cell_verts_coords[k+6];
+      d[k] = cell_verts_coords[k+9];
+      bc[k] = b[k] - c[k];
+      bd[k] = b[k] - d[k];
+      cd[k] = c[k] - d[k];
+      ca[k] = c[k] - a[k];
+      da[k] = d[k] - a[k];
+      ab[k] = a[k] - b[k];
+      p_a[k] = p[k] - a[k];
+      p_b[k] = p[k] - b[k];
+      p_c[k] = p[k] - c[k];
+      p_d[k] = p[k] - d[k];
     }
-  }
-  // example of normal auto normal = normalize(cross((b - a), (c - a))) in r3d_test.cpp;
-  //look at dot and cross in vector.hpp
+    auto bc_v = vector_3(bc[0], bc[1], bc[2]);
+    auto bd_v = vector_3(bd[0], bd[1], bd[2]);
+    auto cd_v = vector_3(cd[0], cd[1], cd[2]);
+    auto ca_v = vector_3(ca[0], ca[1], ca[2]);
+    auto da_v = vector_3(da[0], da[1], da[2]);
+    auto ab_v = vector_3(ab[0], ab[1], ab[2]);
+
+    auto p_a_v = vector_3(p_a[0], p_a[1], p_a[2]);
+    auto p_b_v = vector_3(p_b[0], p_b[1], p_b[2]);
+    auto p_c_v = vector_3(p_c[0], p_c[1], p_c[2]);
+    auto p_d_v = vector_3(p_d[0], p_d[1], p_d[2]);
+
+    auto normal_a = normalize(cross(bc_v,bd_v));
+    auto normal_b = normalize(cross(cd_v,ca_v));
+    auto normal_c = normalize(cross(da_v,-bd_v));
+    auto normal_d = normalize(cross(ab_v,-ca_v));
+
+    auto xi_a = (p_b_v*normal_a)/((bc_v)*normal_a);
+    auto xi_b = (p_c_v*normal_b)/((cd_v)*normal_b);
+    auto xi_c = (p_d_v*normal_c)/((da_v)*normal_c);
+    auto xi_d = (p_a_v*normal_d)/((ab_v)*normal_d);
+  };
+  parallel_for(numPts, f, "barycentric_coords");
+
+  //source for calculation "https://people.sc.fsu.edu/~jburkardt/presentations/cg_lab_barycentric_tetrahedrons.pdf"
   return 0;
 }
