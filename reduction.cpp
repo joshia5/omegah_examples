2efbbac460bb9b5b45f117f974a910b86b8d3710
joshia5 <joshia5@rpi.edu>
2020-02-11 18:05:11 -0500

    with assertion

--- reduction.cpp
+++ reduction.cpp
@@ -2,6 +2,7 @@
 #include <Omega_h_library.hpp>
 #include <Omega_h_mesh.hpp>
 #include <Omega_h_array_ops.hpp>
+#include <Omega_h_for.hpp>
 
 using namespace Omega_h;
 
@@ -11,24 +12,23 @@ int main(int argc, char** argv) {
     fprintf(stderr, "Usage: %s <input mesh>\n", argv[0]);
     return 0;
   }
-  const auto rank = lib.world()->rank();
   const auto inmesh = argv[1];
   Mesh mesh(&lib);
   binary::read(inmesh, lib.world(), &mesh);
-  const auto dim = mesh.dim();
+  const auto rank = lib.world()->rank();
+  const auto comm = lib.world();
+  LO max_input = 45;
 
-  auto faces = mesh.ask_up(1,2).ab2b;
-  auto comm = lib.world();
-  auto max_face = get_max(faces);
-  auto max_face_comm = get_max(comm, faces);
-  std::cout << "\n loc max " << max_face << "\n glo max" << max_face_comm << std::endl;
-  //std::cout << "\n  rank " << rank << ", maxface " << max_face << ", nface " << mesh.nfaces() << ", gface " << mesh.nglobal_ents(2) << std::endl;
-  if (rank == 0) assert(max_face+1 == mesh.nfaces());
-
-  auto vert_coords = mesh.coords();
-  auto max_coord = get_max(vert_coords);
-  auto max_coord_comm = get_max(comm, vert_coords);
-  std::cout << "\n rank " << rank << " coord loc max " << max_coord << "\n glo max" << max_coord_comm << std::endl;
+  Write<LO> for_max(mesh.nfaces(), 0, "for_max");
+  if (!rank) {
+    auto set_max_val = OMEGA_H_LAMBDA(LO i) {
+      if(!i) for_max[i] = max_input;
+    };
+    parallel_for(1, set_max_val, "set_max_val");
+  }
+  Read<LO> for_max_r(for_max);
+  auto max_val = get_max(comm, for_max_r);
+  assert (max_val == max_input);
 
   return 0;
 }
