5cc8ec4a9ecc8d11d80f01b6b873d754e59da4a0
joshia5 <joshia5@rpi.edu>
2020-02-17 17:09:54 -0500

    synchro working

--- synchronization.cpp
+++ synchronization.cpp
@@ -2,7 +2,6 @@
 #include <Omega_h_library.hpp>
 #include <Omega_h_mesh.hpp>
 using namespace Omega_h;
-using namespace std;
 
 int main(int argc, char** argv) {
   auto lib = Library(&argc, &argv);
@@ -11,27 +10,27 @@ int main(int argc, char** argv) {
     return 0;
   }
   const auto rank = lib.world()->rank();
-  const auto comm = lib.world();
   const auto inmesh = argv[1];
   Mesh mesh(&lib);
   binary::read(inmesh, lib.world(), &mesh);
   const auto dim = mesh.dim();
-/*
-  mesh.add_tag<Real>(0, "gravity", 1);
 
-  cout << "\n 1 rank is " << rank << endl;
-  if (rank == 1) {
+  mesh.add_tag<Real>(0, "gravity", 1);
+  if (!rank) {
     Write<Real> gravityArray(mesh.nverts(), 9.81, "gravityArray");
     Read<Real> gravityArray_r(gravityArray);
     mesh.set_tag<Real>(0, "gravity", gravityArray_r);
-    //mesh.sync_tag(0, "gravity");
   }
+  else if (rank) {
+    Write<Real> gravityArray(mesh.nverts(), 0.0, "gravityArray");
+    Read<Real> gravityArray_r(gravityArray);
+    mesh.set_tag<Real>(0, "gravity", gravityArray_r);
+  }
+
   MPI_Barrier(MPI_COMM_WORLD);
-  cout << "\n 2 rank is " << rank << endl;
-  //mesh.sync_tag(0, "gravity");
-  cout << "\n 3 rank is " << rank << endl;
-  //write parallel file
-  //vtk::write_parallel("./partition.vtk", &mesh, dim);
-*/
+  vtk::write_parallel("/users/joshia5/omegah_examples/new_mesh/before_synchronization.vtk", &mesh, false);
+  mesh.sync_tag(0, "gravity");
+  vtk::write_parallel("/users/joshia5/omegah_examples/new_mesh/synchronization.vtk", &mesh, false);
+  
   return 0;
 }
