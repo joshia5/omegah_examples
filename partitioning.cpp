182b6117972f961cbe0a74426cdc31db7bca4009
joshia5 <joshia5@rpi.edu>
2020-03-17 13:56:30 -0400

    changing metric

--- partitioning.cpp
+++ partitioning.cpp
@@ -1,8 +1,9 @@
 #include <Omega_h_file.hpp>
 #include <Omega_h_library.hpp>
 #include <Omega_h_mesh.hpp>
-
+#include <Omega_h_for.hpp>
 using namespace Omega_h;
+
 int main(int argc, char** argv) {
   auto lib = Library(&argc, &argv);
   if(argc!=2) {
@@ -16,12 +17,24 @@ int main(int argc, char** argv) {
   const auto dim = mesh.dim();
 
   mesh.add_tag<Real>(0, "metric", 1);
-  Write<Real> metricArray(mesh.nverts(), rank, "metricArray");
-  auto metricArray_r = Reals(metricArray);
-  mesh.set_tag<Real>(0, "metric", metricArray_r);
 
+  if (!rank) {
+    Write<Real> metricArray(mesh.nverts(), 1, "metricArray");
+    auto write_values = OMEGA_H_LAMBDA(LO i) {
+      if (i == 0) metricArray[i] = 0.5;
+    };
+    parallel_for(mesh.nverts(), write_values);
+    auto metricArray_r = Reals(metricArray);
+    mesh.set_tag<Real>(0, "metric", metricArray_r);
+  }
+  else {
+    Write<Real> metricArray(mesh.nverts(), 1, "metricArray");
+    auto metricArray_r = Reals(metricArray);
+    mesh.set_tag<Real>(0, "metric", metricArray_r);
+  }
+   
   MPI_Barrier(MPI_COMM_WORLD);
-  mesh.sync_tag(0,"metric");
+  //mesh.sync_tag(0,"metric");
   mesh.balance(1);
   vtk::write_parallel("/users/joshia5/new_mesh/balance.vtk", &mesh, false);
 
