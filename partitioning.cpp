14225fc2b3a0757d02ffc94a43b79d82636f7b94
joshia5 <joshia5@rpi.edu>
2020-03-21 21:10:13 -0400

    with imbalance calculation

--- partitioning.cpp
+++ partitioning.cpp
@@ -4,6 +4,16 @@
 #include <Omega_h_array_ops.hpp>
 using namespace Omega_h;
 
+Real get_imbalance(Mesh mesh, Read<Real> weight_array) {
+  auto sum = get_sum(weight_array);
+  Write<Real> sum_w(1,sum);
+  auto sum_r = Reals(sum_w);
+  auto max_sum = get_max(mesh.comm(), sum_r);
+  auto avg_sum = get_sum(mesh.comm(), weight_array)/4;
+  Real imbalance = max_sum/avg_sum;
+  return imbalance;
+}
+
 int main(int argc, char** argv) {
   auto lib = Library(&argc, &argv);
   if(argc!=2) {
@@ -14,6 +24,7 @@ int main(int argc, char** argv) {
   Mesh mesh(&lib);
   binary::read(inmesh, lib.world(), &mesh);
   const auto rank = lib.world()->rank();
+  const auto size = lib.world()->size();
   const auto dim = mesh.dim();
 
   if (!rank) {
@@ -27,14 +38,14 @@ int main(int argc, char** argv) {
     mesh.add_tag<Real>(dim, "weight", 1, weight_r);
   }
    
-  MPI_Barrier(MPI_COMM_WORLD);
   auto weight_array_0 = mesh.get_array<Real>(dim, "weight");
   mesh.balance(weight_array_0);
   auto weight_array_1 = mesh.get_array<Real>(dim, "weight");
-
-  auto sum_0 = get_sum(mesh.comm(),weight_array_0);
-  auto sum_1 = get_sum(mesh.comm(),weight_array_1);
-  assert (sum_0 == sum_1);
-  if (!rank) printf("sum before %3.3f, after %3.3f \n", sum_0, sum_1);
+  auto imbalance_0 = get_imbalance(mesh, weight_array_0);
+  auto imbalance_1 = get_imbalance(mesh, weight_array_1);
+  if (!rank) {
+    printf("Weight imbalance before & after balance call %f, %f \n"
+		    , imbalance_0, imbalance_1);
+  }
   return 0;
 }
